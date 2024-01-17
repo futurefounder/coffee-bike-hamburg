66319a539798a9ceea162bd2839d316fedefc369
Jesse <jsk@posteo.de>
2024-01-17 13:30:42 +0000

    update header

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -84,9 +84,16 @@ export default function Footer() {
               <div>
                 <Link
                   href="/impressum"
-                  className="text-slate-300 transition duration-100 hover:text-coffeeAccent hover:text-coffeeAccent"
+                  className="text-slate-300 text-sm transition duration-100 hover:text-coffeeAccent hover:text-coffeeAccent"
                 >
                   Impressum
+                </Link>{" "}
+                <br />
+                <Link
+                  href="/datenschutz"
+                  className="text-gray-300 text-sm transition duration-100 hover:text-coffeeAccent hover:text-coffeeAccent"
+                >
+                  Datenschutz
                 </Link>
               </div>
 
@@ -109,18 +116,58 @@ export default function Footer() {
             </div>
 
             <nav className="flex flex-col gap-4">
-              <div>
+              <div className="text-sm">
                 <span className="text-gray-300">
-                  <span className="font-bold"> Benjamin Stoltenberg</span>
+                  Benjamin Stoltenberg <br />
                   <br />
-                  Telefon: +49 174 607 92 73 <br />
-                  Email:{" "}
-                  <a
-                    href="mailto:info@coffee-bike-hamburg.de"
-                    className="transition duration-100 hover:text-coffeeAccent hover:text-coffeeAccent"
-                  >
-                    info@coffee-bike-hamburg.de
-                  </a>
+                  <div className="flex items-center mr-3">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      strokeWidth={1.5}
+                      stroke="currentColor"
+                      className="w-4 h-4 mr-2"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M2.25 6.75c0 8.284 6.716 15 15 15h2.25a2.25 2.25 0 0 0 2.25-2.25v-1.372c0-.516-.351-.966-.852-1.091l-4.423-1.106c-.44-.11-.902.055-1.173.417l-.97 1.293c-.282.376-.769.542-1.21.38a12.035 12.035 0 0 1-7.143-7.143c-.162-.441.004-.928.38-1.21l1.293-.97c.363-.271.527-.734.417-1.173L6.963 3.102a1.125 1.125 0 0 0-1.091-.852H4.5A2.25 2.25 0 0 0 2.25 4.5v2.25Z"
+                      />
+                    </svg>{" "}
+                    &nbsp;Telefon <br />
+                    <a
+                      className="transition duration-100 hover:text-coffeeAccent hover:text-coffeeAccent"
+                      href="+49 174 607 92 73"
+                    >
+                      {" "}
+                      &nbsp; +49 174 607 92 73{" "}
+                    </a>
+                  </div>
+                  <div className="flex items-center mr-3">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      strokeWidth={1.5}
+                      stroke="currentColor"
+                      className="w-5 h-5 mr-2"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M16.5 12a4.5 4.5 0 1 1-9 0 4.5 4.5 0 0 1 9 0Zm0 0c0 1.657 1.007 3 2.25 3S21 13.657 21 12a9 9 0 1 0-2.636 6.364M16.5 12V8.25"
+                      />
+                    </svg>{" "}
+                    &nbsp;Email <br />
+                    <a
+                      className="whitespace-nowrap transition duration-100 hover:text-coffeeAccent hover:text-coffeeAccent"
+                      href="info@kaffee-bike-hamburg.de"
+                    >
+                      {" "}
+                      &nbsp; info@kaffee-bike-hamburg.de{" "}
+                    </a>
+                  </div>
                 </span>
               </div>
             </nav>
@@ -128,22 +175,7 @@ export default function Footer() {
           {/* <!-- nav - end --> */}
 
           {/* <!-- nav - start --> */}
-          <div>
-            <div className="mb-4 font-bold uppercase tracking-widest text-slate-100">
-              Rechtliches
-            </div>
 
-            <nav className="flex flex-col gap-4">
-              <div>
-                <a
-                  href="/"
-                  className="text-gray-300 transition duration-100 hover:text-coffeeAccent hover:text-coffeeAccent"
-                >
-                  Datenschutz
-                </a>
-              </div>
-            </nav>
-          </div>
           {/* <!-- nav - end --> */}
         </div>
 
